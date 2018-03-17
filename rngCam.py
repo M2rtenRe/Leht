5067469dc5196d8619a929195f89066f7d44cb98
M2rtenRe <marten.reinaas@gmail.com>
2018-03-17 14:48:33 +0200

    gssd

--- rngCam.py
+++ rngCam.py
@@ -11,10 +11,10 @@ def captureCam(time):
         count += 1
         imageCount = "shot"+str(count)+".jpg"
     imageDir = "/home/m2rtenreinaasoriginal/Pictures/"+str(imageCount)
-    #try:
-    imgResp = urllib.request.urlopen(imgUrl)
-    #except:
-    #return "No connection"
+    try:
+        imgResp = urllib.request.urlopen(imgUrl)
+    except:
+        return "No connection"
     imgNp = np.array(bytearray(imgResp.read()),dtype=np.uint8)
     img = cv2.imdecode(imgNp,-1)
     cv2.imwrite(imageDir, img)
