1428f7779c666316fa2087a61fe72549439b2818
M2rtenRe <marten.reinaas@gmail.com>
2018-03-17 14:47:45 +0200

    asafs

--- muteUser.py
+++ muteUser.py
@@ -14,7 +14,7 @@ if command == "mute":
             if line == "alias":
                 sys.exit()
     with io.open("/home/m2rtenreinaasoriginal/banned.txt", "a", encoding='utf-8') as f:
-        f.write(alias)
+        f.write(alias+"\n")
 
 if command == "unmute":
     f = io.open("/home/m2rtenreinaasoriginal/banned.txt", "r", encoding='utf-8')
@@ -26,4 +26,3 @@ if command == "unmute":
         if line != alias:
             f.write(line)
     f.close()
-
